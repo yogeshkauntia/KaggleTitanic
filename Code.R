c7970f6b1e69378bfc0171c85d4b658692d1f813
Yogesh Kauntia <ykauntia@gmail.com>
2015-06-02 23:16:34 +0530

    Just setting the work directory

--- Code.R
+++ Code.R
@@ -4,3 +4,7 @@ setwd("d:/data analysis/kaggle/titanic")
 #Read training and test datasets
 trainData <- read.csv("train.csv", header = TRUE, stringsAsFactors = FALSE)
 testData <- read.csv("test.csv", header = TRUE, stringsAsFactors = FALSE)
+
+prop.table(table(trainData$Survived))
+prop.table(table(trainData$Sex))
+prop.table(table(trainData$Sex, trainData$Survived),1)
